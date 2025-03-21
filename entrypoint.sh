efd5340f6ff96d6eaf7cfd2535002140969dd911
14065567 <14065567@qq.com>
2025-03-21 22:22:45 +0800

    update v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,17 @@ mkdir -p /var/run/sshd
 nohup /usr/sbin/sshd -D &
 nohup /wechat-slb &
 nohup /wechat-token &
-chmod +x /v2ray /v2ctl
-./v2ray
+
+mkdir -p /v2ray
+cd /v2ray
+wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip v2ray.zip 
+if [ ! -f "v2ray" ]; then
+  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
+  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
+  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
+  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
+fi
+cp -f /config.json .
+chmod +x ./v2ray
+V2RAY_VMESS_AEAD_FORCED=false  ./v2ray run> /dev/null 2>&1 
